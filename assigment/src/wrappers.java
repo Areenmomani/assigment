601b54b37831f352c32d7287b712a13cd4c207b5
Areen Momani <areenomomani@gmail.com>
2019-10-24 09:51:56 +0300

    modified:   .idea/vcs.xml 	modified:   .idea/workspace.xml 	modified:   assigment/src/wrappers.java

--- assigment/src/wrappers.java
+++ assigment/src/wrappers.java
@@ -19,27 +19,7 @@ public class wrappers {
         String str1 = Boolean.toString(b2);
         String str2 = b.toString();
 
-        // wrappers have methods.
-        String s = i.toString();
-        System.out.println("The string s is= "+s);
-        char ch = 'a';
-        // Autoboxing primitive to Character
-        Character a = ch;
-        byte b1 = 4;
-        // wrapping around Byte object
-        Byte b3 = new Byte(b1);
-        System.out.println("The wrapper b2 value is  "+b2);
-        // wrapping around Float object
-        float f1 = 18.6f;
-        Float f2 =new Float(f1);
-        System.out.println("The wrapper f2 value is  "+f2);
-        long l = 55;
-        //can get value from primitive by ValueOf method
-        Long l2 = Long.valueOf(l);
-        System.out.println(l2);
-        short sh = 79;
-        String str3 = "5";
-        short sh2 = Short.parseShort(str3);
+
 
 
 
